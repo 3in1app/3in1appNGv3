1b1c702ca8efcf1207369c48cbb9f295c4b4184a
ivanooi <ivanooi@localhost>
2024-09-04 22:38:25 +0800

    Commit message goes here

---
+++ README.md
@@ -0,0 +1,9 @@
+# 3in1appngv3
+
+{{ appDescription }}
+
+[Releases](https://github.com/3in1app/3in1appNGv3/releases)
+
+## About this Project
+
+This project was generated using the Swing project template from [jDeploy](https://www.jdeploy.com), a tool to deploy Java desktop apps on Windows, Mac.
---
+++ src/test/java/com/github/3in1app/3in1appngv3/3in1appNGv3Test.java
@@ -0,0 +1,38 @@
+package com.github.3in1app.3in1appngv3;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class 3in1appNGv3Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public 3in1appNGv3Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( 3in1appNGv3Test.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
